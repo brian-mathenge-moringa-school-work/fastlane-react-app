a44f9da7c1c9ec24f851349f0bc4cba597580679
Brian <wamwangimathenge@gmail.com>
2023-03-30 14:45:35 +0300

    Code refactored

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React from "react";
 import "./App.css"
 import Home from "./Components/Home";
+import NavBar from './Components/NavBar';
+import Cars from './Components/Cars';
+import Footer from './Components/Footer'
+import CarDetails from './Components/CarDetails'
+import Signup from './Components/Signup'
+import Cart from './Components/Cart';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import "../src/App.css"
 
 function App() {
+
   return (
     <div className="App">
-      <Home />
+      <Router>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/cars' element={<Cars />}/>
+      <Route path='/cars/:id' element={<CarDetails />}/>
+      <Route path='/signup' element={<Signup />}/>
+      <Route path='/cart' element={<Cart />}/>
+    </Routes>
+    <Footer />
+  </Router>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,31 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import NavBar from './Components/NavBar';
-import Cars from './Components/Cars';
-import Footer from './Components/Footer'
-import CarDetails from './Components/CarDetails'
-import Signup from './Components/Signup'
-import Cart from './Components/Cart';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import "../src/App.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  
-  <Router>
-    <Routes>
-      <Route path='/' element={<App />}/>
-      <Route path='/cars' element={<Cars />}/>
-      <Route path='/cars/:id' element={<CarDetails />}/>
-      <Route path='/signup' element={<Signup />}/>
-      <Route path='/cart' element={<Cart />}/>
-    </Routes>
-    <Footer />
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
